a4a4f8976369db74b9b86640562ec428661181a6
Flouis2017 <315587951@qq.com>
2018-07-17 23:28:35 +0800

    SSM integration

---
+++ src/main/java/com/happyshopping/test/MyTest.java
@@ -0,0 +1,12 @@
+package com.happyshopping.test;
+
+import org.junit.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test(){
+		System.out.println(Thread.currentThread().getName());
+	}
+
+}
